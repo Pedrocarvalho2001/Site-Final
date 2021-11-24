a8b1e798d5b547fe60a621a38d72a75edbecb65f
Pedrocarvalho2001 <89496667+Pedrocarvalho2001@users.noreply.github.com>
2021-11-24 10:06:26 -0300

    Add files via upload

---
+++ cadastro/script.js
@@ -0,0 +1,8 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid", "sobreid", "ageid");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' , suas informações foram armazenadas com sucesso');
+    }
+}
